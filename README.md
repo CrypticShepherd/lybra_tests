d9beb2b030fc48a7360d965cb198911bb1d75028
CrypticShepherd <cryptic.shepherd@gmail.com>
2023-06-30 12:51:05 +0200

    Added POC Framework, updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# lybra_tests
-Tests for Lybra Protocol V2
+# Lybra v2 Test Setup
+
+Test setup with Foundry. For the POCs (delivered seperately) to work, the vault interface in LybraConfigurator has to be fixed, see my issue report on this.
+
+Copy POCS into test/TestPOCFramework.t.sol
---
+++ test/TestPOCFramework.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+
+import "forge-std/Test.sol";
+import {TestsBase} from "./TestsBase.t.sol";
+
+contract TestsPOC is TestsBase {
+    function setUp() public virtual override {
+        TestsBase.setUp();
+    }
+
+    // COPY POCS HERE
+
+}
